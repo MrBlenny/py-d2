41754e60ff261ae12fb02c9ff4aaccef30437d0b
David Revay <daverevay@gmail.com>
2024-10-10 15:20:02 +1100

    chore: order imports

--- src/py_d2/__init__.py
+++ src/py_d2/__init__.py
@@ -1,8 +1,9 @@
 from .connection import D2Connection
 from .connection import Direction
 from .diagram import D2Diagram
-from .shape import D2Shape, Shape
+from .shape import D2Shape
 from .shape import D2Text
+from .shape import Shape
 from .style import D2Style
 
 
