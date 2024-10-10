6d11b49ce3e23942d4dfc45573b7f6d859a84493
David Revay <daverevay@gmail.com>
2024-10-10 15:07:56 +1100

    chore: export Shape

--- src/py_d2/__init__.py
+++ src/py_d2/__init__.py
@@ -1,7 +1,7 @@
 from .connection import D2Connection
 from .connection import Direction
 from .diagram import D2Diagram
-from .shape import D2Shape
+from .shape import D2Shape, Shape
 from .shape import D2Text
 from .style import D2Style
 
@@ -13,4 +13,5 @@ __all__ = [
     "D2Shape",
     "D2Text",
     "D2Style",
+    "Shape"
 ]
